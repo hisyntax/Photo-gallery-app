bdf4b6d0deccc466f2571f8df1cb280d66bb9cdb
hisyntax <hisyntax0@gmail.com>
2021-07-16 00:05:24 +0100

    Added a method() to the router

--- main.go
+++ main.go
@@ -44,10 +44,10 @@ func main() {
 	faqView = views.NewView("bootstrap", "views/faq.html")
 	usersC := controllers.NewUsers()
 	r := mux.NewRouter()
-	r.HandleFunc("/", home)
-	r.HandleFunc("/contact", contact)
-	r.HandleFunc("/faq", faq)
-	r.HandleFunc("/signup", usersC.New)
+	r.HandleFunc("/", home).Methods("GET")
+	r.HandleFunc("/contact", contact).Methods("GET")
+	r.HandleFunc("/faq", faq).Methods("GET")
+	r.HandleFunc("/signup", usersC.New).Methods("GET")
 	http.ListenAndServe(":3000", r)
 
 }
