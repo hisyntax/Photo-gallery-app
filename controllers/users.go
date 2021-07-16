da2690a47d632e6f79c48514a91e9b298b407eab
hisyntax <hisyntax0@gmail.com>
2021-07-16 01:07:04 +0100

    Using gorilla/schema to handle the form

--- controllers/users.go
+++ controllers/users.go
@@ -4,6 +4,8 @@ import (
 	"fmt"
 	"net/http"
 	"src/github.com/gorilla/mux/views"
+
+	"github.com/gorilla/schema"
 )
 
 type Users struct {
@@ -16,6 +18,11 @@ func NewUsers() *Users {
 	}
 }
 
+type SignupForm struct {
+	Email    string `schema:"email"`
+	Password string `schema:"password"`
+}
+
 func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 	if err := u.NewView.Render(w, nil); err != nil {
 		panic(err)
@@ -23,5 +30,14 @@ func (u *Users) New(w http.ResponseWriter, r *http.Request) {
 }
 
 func (u *Users) Create(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintln(w, "This is a temporary response.")
+	if err := r.ParseForm(); err != nil {
+		panic(err)
+	}
+
+	dec := schema.NewDecoder()
+	form := SignupForm{}
+	if err := dec.Decode(&form, r.PostForm); err != nil {
+		panic(err)
+	}
+	fmt.Fprintln(w, form)
 }
